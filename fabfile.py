2e1a8b40e73b6610eedb649d1ede5c2f231f3df1
caojian <647812411@qq.com>
2019-12-30 16:35:17 +0800

    update

---
+++ fabfile.py
@@ -0,0 +1,110 @@
+import argparse, sys
+from fabric.api import *
+from config.config import *
+
+parser = argparse.ArgumentParser(description='deploys duang!!!')
+
+# parser.add_argument(dest='pro_name', type=str)
+# parser.add_argument(dest='env_name', type=str)
+# parser.add_argument(dest='parameters', nargs='*')
+
+# args = parser.parse_args()
+# print(args)
+env_name = 'dev'
+pro_name = 'api'
+parameters = []
+
+host_info = REMOTE_HOSTS[env_name]
+env.host = host_info.get('host')
+env.port = host_info.get('port')
+env.user = host_info.get('user')
+env.password = host_info.get('password')
+exclude_files = EXCLUDE_FILES[pro_name]
+host_info = host_info
+
+git_path = ("%s/project/%s/git" % (sys.path[0], pro_name))
+# version = parameters[1]
+remote_path = REMOTE_PATH[env_name][pro_name]
+local_path = git_path + '/' + PRO_DIR_MAP[pro_name]
+pro_name_dir = PRO_DIR_MAP[pro_name]
+
+'''
+制作归档包tar
+'''
+def make_package(version):
+    cmd = 'sh ' + git_path + 'mk_dev_main_bsb.sh ' + version
+    local(command=cmd)
+
+
+'''
+下载排除文件
+'''
+def download():
+    remote_dir = remote_path
+    local_dir = local_path
+    for file in exclude_files:
+        get(remote_path=remote_dir + "/" + file, local_path=local_dir + "/" + file)
+
+
+'''
+打包
+'''
+
+
+def tar():
+    target_dir = local_path
+    name = pro_name_dir + '.tar'
+    local("tar -cf " + name + " " + target_dir)
+
+
+'''
+解包
+'''
+def untar( name):
+    local("tar -xf " + name)
+
+
+'''
+上传文件
+'''
+def upload():
+    local_paths = local_path + '/' + pro_name_dir + '.tar'
+    remote_paths = remote_path
+    put(local_path=local_paths, remote_path=remote_paths)
+
+
+'''
+执行远程命令
+'''
+
+
+def myrun():
+    lcd(remote_path)
+    run("tar -xf " + pro_name_dir + '.tar')
+
+
+'''
+备份
+'''
+
+
+def backup(self):
+    pass
+
+
+'''
+回滚
+'''
+
+
+def rollback():
+    pass
+
+
+'''
+部署
+'''
+def deploy():
+    download()
+    upload()
+    myrun()
---
+++ payment.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/30 15:39
+# @Author  : Tonny Cao
+# @Email   : 647812411@qq.com
+# @File    : payment.py
+# @Software: PyCharm
